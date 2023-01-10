d02c957d3f07cd4cae70dc9ac739c684f9e65001
Mateusz Byrtus <mateusz.byrtus@gmail.com>
2023-01-10 19:37:46 +0100

    added icons and weather description

--- main.js
+++ main.js
@@ -4,24 +4,34 @@ const appId = import.meta.env.VITE_APPID;
 const button = document.getElementById("search-location");
 const input = document.getElementById("location");
 const output = document.getElementById("output");
+const header = document.getElementById("header");
 button.addEventListener("click", getCurrentWeather);
 
 async function getCurrentWeather(e) {
   e.preventDefault();
   output.innerHTML = "";
-
-  const url = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${appId}`
-  );
-  const weather = await url.json();
-  console.log(weather);
-  const cityNode = document.createElement("div");
-  cityNode.innerHTML = `<h1 class="text-lg">Weather in the ${weather.name}</h1> 
-  <dt class="font-medium text-gray-900">Temperature</dt>
-          <dd class="mt-2 text-sm text-gray-500">${Math.round(
-            weather.main.temp - 273.15
-          )} °C</dd>
+  try {
+    const url = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${appId}`
+    );
+    header.style.display = "none";
+    let response = await url.json();
+    const cityNode = document.createElement("div");
+    cityNode.innerHTML = `<h1 class="text-lg pt-16">Weather in ${
+      response.name
+    }</h1> 
+  <dt class="font-medium text-gray-900">${Math.round(
+    response.main.temp - 273.15
+  )} °C ${response.weather[0].description} </dt>
+          <dd class="mt-2 text-sm text-gray-500"><img src=https://openweathermap.org/img/wn/${
+            response.weather[0].icon
+          }@2x.png></dd>
 `;
-  output.appendChild(cityNode);
-  input.value = "";
+    output.appendChild(cityNode);
+  } catch (error) {
+    header.style.display = "block";
+    header.textContent = "No such a location";
+  } finally {
+    input.value = "";
+  }
 }
